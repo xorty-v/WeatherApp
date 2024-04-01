aebd4a1499e6ac2608fdb0c5194c3ff9f24bc7e8
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-04-01 08:26:32 +0400

    add project

---
+++ main.js
@@ -0,0 +1,70 @@
+const apiKey = '88098331a3154bc4b38101539243103';
+
+const header = document.querySelector('.header');
+const form = document.querySelector('#form');
+const input = document.querySelector('#inputCity');
+
+
+function removeCard() {
+    const prevCard = document.querySelector('.card');
+    if (prevCard) prevCard.remove();
+}
+
+function showError(errorMessage) {
+    const html = `<div class="card">${errorMessage}</div>`;
+    header.insertAdjacentHTML('afterend', html);
+}
+
+function showCard({ name, country, temp, condition, imgPath }) {
+    const html = `<div class="card">
+                                <h2 class="card-city">${name} <span>${country}</span></h2>
+
+                                <div class="card-weather">
+                                    <div class="card-value">${temp}<sup>°c</sup></div>
+                                    <img class="card-img" src="${imgPath}" alt="WeatherIcon">
+                                </div>
+
+                                <div class="card-description">${condition}</div>
+                            </div>`;
+
+    header.insertAdjacentHTML('afterend', html);
+}
+
+async function getWeather(city) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+
+form.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let city = input.value.trim();
+    const data = await getWeather(city);
+
+    if (data.error) {
+        removeCard();
+        showError(data.error.message);
+    }
+    else {
+        removeCard();
+
+        const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
+        const fileName = data.current.condition.code + '.png';
+
+        const imgPath = filePath + fileName;
+
+        const weatherData = {
+            name: data.location.name,
+            country: data.location.country,
+            temp: data.current.temp_c,
+            condition: data.current.condition.text,
+            imgPath
+        };
+
+        showCard(weatherData);
+    }
+}
